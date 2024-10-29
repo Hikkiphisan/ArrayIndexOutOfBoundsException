92fe84f41b3716719c89248f9d741c1ee7026d9a
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-10-29 16:23:13 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui long nhap chi so cua mot phan tu bat ky: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số 5" + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("vuot qua gioi han");
+            e.printStackTrace();
+        }
+    }
+}
